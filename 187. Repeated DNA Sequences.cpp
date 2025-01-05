eb7831e7d857bcc4d782cffb66178dc8528c6d76
Vishal Meharwade <vishalmeharwade1@gmail.com>
2025-01-05 20:24:13 +0530

    Create 187. Repeated DNA Sequences.cpp

---
+++ 187. Repeated DNA Sequences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        
+        unordered_map<string, int> mpp;
+        vector<string> res;
+
+        for(int i=0; i+10 <= s.size(); i++) {
+            mpp[s.substr(i, 10)]+=1;
+        }
+
+        for(auto& [key, value] : mpp) {
+            if(value > 1) res.push_back(key);
+        }
+
+        return res;
+    }
+};
